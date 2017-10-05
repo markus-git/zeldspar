558473afefe2556addda04bf73071ba37ab80e20
Markus <mararon@chalmers.se>
2017-10-05 11:45:01 +0200

    filled in more examples.

--- examples/ZeldsparTest.hs
+++ examples/ZeldsparTest.hs
@@ -2,7 +2,7 @@ module ZeldsparTest where
 
 import Zeldspar
 
-import Prelude hiding (take)
+import Prelude hiding (head, take, drop, reverse, sum)
 
 -- co-feldspar.
 import Feldspar.Software as Soft hiding (loop)
@@ -31,14 +31,14 @@ stored :: ZS (SExp Int32) (SExp Int32) ()
 stored = prog1 >>> loop store >>> prog2
 
 --------------------------------------------------------------------------------
-{-
-prog3 :: Zun (Data Int32) (Data Int32) ()
+
+prog3 :: ZS (SExp Int32) (SExp Int32) ()
 prog3 = loop $ do
     i <- take
     emit (i + 1)
     emit (i + 2)
 
-prog4 :: Zun (Data Int32) (Data Int32) ()
+prog4 :: ZS (SExp Int32) (SExp Int32) ()
 prog4 = loop $ do
     i <- take
     emit (i * 2)
@@ -47,50 +47,44 @@ prog4 = loop $ do
     i <- take
     emit (i * 4)
 
-infinite :: Zun (Data Int32) (Data Int32) ()
+infinite :: ZS (SExp Int32) (SExp Int32) ()
 infinite = prog3 >>> prog4
 
----
-
-infinite' :: Zun (Data Int32) (Data Int32) ()
+infinite' :: ZS (SExp Int32) (SExp Int32) ()
 infinite' = (emit 13 >> prog3) >>> prog4
 
----
+--------------------------------------------------------------------------------
 
-vecMake :: Zun (Data Int32) (DPull Int32) ()
+type SPull a = Pull Software (SExp a)
+
+vecMake :: ZS (SExp Int32) (SPull Int32) ()
 vecMake = loop $ do
     i <- take
     emit $ fmap (i2n) (0 ... i2n i)
 
-vecInc :: (PrimType a, Num a) => Zun (DPull a) (DPull a) ()
+vecInc :: ZS (SPull Int32) (SPull Int32) ()
 vecInc = loop $ do
     v <- take
     emit (fmap (+1) v)
 
-vecRev :: PrimType a => Zun (DPull a) (DPull a) ()
+vecRev :: ZS (SPull Int32) (SPull Int32) ()
 vecRev = loop $ do
     v <- take
     emit (reverse v)
 
-vecTail :: Zun (DPull Int32) (DPull Int32) ()
+vecTail :: ZS (SPull Int32) (SPull Int32) ()
 vecTail = loop $ do
     v <- take
     lift $ printf "Dropping: %d\n" (head v)
     emit (drop 1 v)
 
-vecSum :: Zun (DPull Int32) (Data Int32) ()
+vecSum :: ZS (SPull Int32) (SExp Int32) ()
 vecSum = loop $ do
     v <- take
     emit (sum v)
 
-fusedVec = vecMake >>> vecInc >>> vecRev >>> vecTail >>> vecSum
-
-storedVec = vecMake >>> vecInc >>> loop store >>> vecRev >>> vecTail >>> vecSum
-
-parVec = (vecMake >>> vecInc) |>>10`ofLength`30>>| (vecRev >>> (vecTail >>> vecSum))
-
-parVec' n = vecMake |>>4`ofLength`n>>| vecTail |>>3`ofLength`n>>| vecSum |>>2>>| prog1
--}
+--fusedVec  = vecMake >>> vecInc >>> vecRev >>> vecTail >>> vecSum
+--storedVec = vecMake >>> vecInc >>> loop store >>> vecRev >>> vecTail >>> vecSum
 
 --------------------------------------------------------------------------------
 -- *
@@ -101,5 +95,9 @@ prepare p = runZ p src snk
   where
     src = fget stdin
     snk = printf "%d\n"
+
+scompile :: ZS (SExp Int32) (SExp Int32) () -> IO ()
+scompile = Soft.icompile . prepare
+
 
 --------------------------------------------------------------------------------
